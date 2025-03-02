44e50f6b7f5d6eac5d4c9e11c17889ac764ae07e
coder4434 <tumerabdurrahman31@gmail.com>
2025-03-02 22:52:57 +0300

    first

--- MovieProject/Models/Film.cs
+++ MovieProject/Models/Film.cs
@@ -1,21 +1,46 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace MovieProject.Models
 {
     public class Film
     {
         public int Id { get; set; }
+
+        [Required, MinLength(2), MaxLength(100)]
         public string AdiTurkce { get; set; }
+
+        [Required, MinLength(2), MaxLength(100)]
         public string AdiIngilizce { get; set; }
+
+        [Required]
+        [Range(typeof(DateTime), "1900-01-01", "2030-12-31", ErrorMessage = "Tarih 1900 ile 2030 arasında olmalıdır.")]
         public DateTime CikisTarihi { get; set; }
+
+        [Required, Url(ErrorMessage = "Geçerli bir URL girin.")]
         public string imgURL { get; set; }
+
+        [Required, Url(ErrorMessage = "Geçerli bir URL girin.")]
         public string backimgURL { get; set; }
-        public string sure { get; set; }
+
+        [Required]
+        [Range(1, 500, ErrorMessage = "Film süresi 1 ile 500 dakika arasında olmalıdır.")]
+        public int sure { get; set; }
+
+        [Required, MinLength(3, ErrorMessage = "Kategori en az 3 karakter olmalıdır.")]
         public string categories { get; set; }
+
+        [Required, MinLength(10, ErrorMessage = "Açıklama en az 10 karakter içermelidir.")]
         public string description { get; set; }
-        //public double ImdbPuan { get; set; }
-        public decimal ImdbPuan { get; set; }
+
+        [Required]
+        [Range(0.0, 10.0, ErrorMessage = "IMDb puanı 0 ile 10 arasında olmalıdır.")]
+        public double ImdbPuan { get; set; }
+
+        [Required, MinLength(3)]
         public string Oyuncular { get; set; }
+
+        [Required, MinLength(3)]
         public string Yapimci { get; set; }
     }
 }
